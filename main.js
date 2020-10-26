50f35835926f969efdf0853a505f32700b125242
b-chavez <brandedhard@gmail.com>
2020-10-25 20:18:45 -0700

    Reset now runs randomColor again

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 var colorNumber = document.querySelector("#colorNumber");
 
+colorChange()
+
+var resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener('click', function(){
+    colorChange()
+})
+
 function setButtonColor(button, red, green, blue){
     button.setAttribute('style', 'background-color: rgb(' + red + ',' + green + ',' + blue + ');')
 }
-
-var buttons = document.querySelectorAll('.colors');
-
-
-var answerButton = Math.round(Math.random() * (buttons.length - 1))
-for(var i = 0; i < buttons.length; i++){
-    var red = getColorValue();
-    var blue = getColorValue();
-    var green = getColorValue();
-
-    setButtonColor(buttons[i], red, blue, green);
-
-    if(i === answerButton){
-        colorNumber.innerHTML = `(${red}, ${green}, ${blue})`;
-    }
-}
-
 
 function getColorValue(){
     return Math.floor(Math.random()*255)
 }
 
+function colorChange(){
+    var buttonsList = document.querySelectorAll('.colors');
+    var answerButton = Math.round(Math.random() * (buttonsList.length - 1))
+    for(var i = 0; i < buttonsList.length; i++){
+        var red = getColorValue();
+        var blue = getColorValue();
+        var green = getColorValue();
+    
+        setButtonColor(buttonsList[i], red, blue, green);
+    
+        if(i === answerButton){
+            colorNumber.innerHTML = `(${red}, ${green}, ${blue})`;
+        }
+    }
+}
